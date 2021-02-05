c6e30456e266b2988526a65bde9858a6f95f9355
Maria <maria.mihaila.17@gmail.com>
2021-02-05 10:49:46 +0200

    Create containers for the courses display on page.

--- client/src/Courses.js
+++ client/src/Courses.js
@@ -32,14 +32,20 @@ class Courses extends React.Component {
         return (
        <div>
             <Header />
-            <ul>
+            <div className="bounds">
                 {courses.map(course => (
-                    <li key={course.id}>
-                        {course.title}
-                    </li>
-                ))}
-            </ul>
-        </div>
+                <div className="grid-33"><a className="course--module course--link" href="course-detail.html">
+                    <h4 className="course--label" key={course.id}></h4>
+                    <h3 className="course--title">{course.title}</h3>
+                </a></div>
+                       ))}
+            <div className="grid-33"><a className="course--module course--add--module" href="create-course.html">
+                <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add">
+                    <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 " />
+                        </svg>New Course</h3>
+                </a></div>
+            </div>
+       </div>
         )
     }
 }
