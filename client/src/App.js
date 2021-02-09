83e3d7b49fe29e1c7543b7b22445f84d186247c4
Maria <maria.mihaila.17@gmail.com>
2021-02-09 12:09:32 +0200

    Add the coursesUpdate component to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import CourseDetail from './CourseDetail';
 import UserSignIn from "./UserSignIn";
 import UserSignUp from "./UserSignUp";
 import createCourse from "./create-course";
+import updateCourse from "./update-course";
 
 
 
@@ -22,7 +23,8 @@ export default () => (
               <Route path='/courses/details/:id' component={CourseDetail}/>
               <Route path='/signin' component={UserSignIn}/>
               <Route path='/signup' component={UserSignUp}/>
-              <Route patch='/createcourse' component={createCourse}/>
+              <Route path='/createCourse' component={createCourse}/>
+              <Route path='/courses/update' component={updateCourse}/>
             </Switch>
           </div>
         </header>
