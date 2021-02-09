67dc5c7a965151609e39b3e12fc3d0264156cfe8
Maria <maria.mihaila.17@gmail.com>
2021-02-09 13:43:32 +0200

    Update newCourse button from main Courses list, to redirect to the create-course page

--- client/src/Courses.js
+++ client/src/Courses.js
@@ -39,7 +39,7 @@ class Courses extends React.Component {
                     <h3 className="course--title">{course.title}</h3>
                 </a></div>
                        ))}
-            <div className="grid-33"><a className="course--module course--add--module" href="create-course.html">
+            <div className="grid-33"><a className="course--module course--add--module" href="/courses/create">
                 <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add">
                     <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 " />
                         </svg>New Course</h3>
